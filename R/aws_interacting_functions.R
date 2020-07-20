39bbf1d77fa0dff14d6a8ea34dce52ff3e13a798
mshadbolt <mshadbolt@ebi.ac.uk>
2020-07-20 16:26:41 +0100

    migrated aws functions to hca-util

--- R/aws_interacting_functions.R
+++ R/aws_interacting_functions.R
@@ -5,105 +5,120 @@
 #' The user must already have permissions to access the EC2 already set up.
 #'
 #' @param username string
-#' @param ec2_url ec2 url, `tool.staging.data.humancellatlas.org` by default
+#' @param ec2_url ec2 url, `tool.archive.data.humancellatlas.org` by default
 #' @return connection ssh connection object
 #' @export
-ssh_ec2 <- function(username, ec2_url="tool.staging.data.humancellatlas.org") {
+ssh_ec2 <- function(username, ec2_url="tool.archive.data.humancellatlas.org") {
   user_url <- paste0(username, "@", ec2_url)
   connection <- ssh::ssh_connect(user_url)
   return(connection)
+}
+
+
+#' Set up hca-util
+#'
+#' \code{setup_hca_util} creates a virtual environment and installs hca-util
+#' @export
+setup_hca_util <- function(){
+  reticulate::virtualenv_create(envname="ranglr_env")
+  reticulate::virtualenv_install(envname="ranglr_env", packages = "hca-util",
+                                 pip_options = "--upgrade")
+  system(paste0(reticulate::virtualenv_root(), "/ranglr_env/bin/hca-util -h"))
+  if (! "[profile hca-util]" %in% read.delim("~/.aws/config", header = F)$V1){
+    message("hca-util probably needs to be configured")
+    message("follow the instructions here: https://github.com/ebi-ait/hca-documentation/wiki/How-to-administrate-upload-areas-using-hca-util#configuration")
+  }
 }
 
 #' List files in S3
 #'
 #' \code{list_s3_files} lists the s3 files in a given s3 bucket.
 #'
-#' @param s3_url the url of the s3 bucket, e.g. s3://org-humancellatlas-upload-staging/aaaaaaaa-bbbb-cccc-dddd...
-#' @param user the user's username usually `FirstInitialLastName`
+#' Assumes you have installed and configured the \code{setup_hca_util} command.
+#' Can optionally provide the path to where you have installed hca-util
+#'
+#' @param s3_uuid the url of the s3 upload area.
+#' @param hca_util_path the path to the hca-util installation
 #' @return a tibble of files in bucket
 #' @export
 # TODO: write code to ensure it doesn't error or at least hide the error message
-list_s3_files <- function(s3_url, user) {
+list_s3_files <- function(s3_uuid,
+                          hca_util_path = paste0(reticulate::virtualenv_root(),
+                                                 "/ranglr_env/bin/hca-util")){
   tryCatch({
-    this_connection <- ssh_ec2(username = user)
-    this_command <- paste0("aws s3 ls ", s3_url)
-    out <- ssh::ssh_exec_internal(this_connection,
-                                  command = this_command)
-    ssh::ssh_disconnect(this_connection)
-    filename_df <- stringr::str_split(rawToChar(out$stdout), "\\n",
-                                      simplify = T) %>%
-      as.data.frame() %>%
-      t() %>%
-      tibble::as_tibble() %>%
-      tidyr::separate(col = V1, sep = "[[:space:]]+",
-                      into = c("date", "time", "size", "file_name")) %>%
-      dplyr::filter(!is.na(file_name))
+    this_select_command <- paste0(hca_util_path, " select ", s3_uuid)
+    this_list_command <- paste0(hca_util_path, " list")
+    out_select <- system(command = this_select_command)
+    out_list <- system(command = this_list_command, intern = TRUE)
+    filename_df <- out_list %>%
+      tibble::enframe() %>%
+      tidyr::separate(col = value, sep = "/",
+                      into = c("uuid", "file_name")) %>%
+      dplyr::filter(!is.na(file_name)) %>%
+      dplyr::select(file_name)
+    filename_df <- filename_df[-1,]
     return(filename_df)
   },
   error=function(cond){
     message("s3 probably empty, original error message:")
     message(cond)
-    ssh::ssh_disconnect(this_connection)
     return(NA)
   }
   )
-
-
 }
 
 #' Create s3 upload area
 #'
-#' \code{create_s3} creates an s3 upload area for a particular project. By
-#' assumes you have the staging api key saved in your .Renviron file and called
-#' "STAGING_UPLOAD_API_KEY"
+#' \code{create_s3} uses the hca-util tool to create an s3 upload area for a
+#' particular project.
 #'
-#' @param user your username
-#' @param project_shorthand the `-` delimited name of the dataset e.g. my-cool-project
-#' @param upload_api_key the upload api string
-#' @param upload_env_name the name of the environment, i.e. staging, dev,
-#' integration
-#' @param submission_n by default 0 but can be changed if it is a bucket for
-#'  a subsequent submission for the same project
-#' @return the url of the s3 bucket of the form \code{s3://org-humancellatlas-upload-staging/aaaaaaaa-bbbb-cccc-dddd-e012345f6789/}
-#' @examples
-#' create_s3("FirstInitialLastName", "my-cool-project")
+#' Assumes you have installed and configured the \code{setup_hca_util} command.
+#' Can optionally provide the path to where you have installed hca-util
+#'
+#' @param project_shorthand the `shortname of the dataset e.g. my-cool-project
+#' @param hca_util_path the path to installed hca-util
+#' @param permissions allowed actions (permissions) on new area. u for upload, x
+#'  for delete and d for download. Default is ux
+#' @param profile specify name of admin profile, otherwise uses default
+#' @return the uuid of the s3 bucket
 #' @export
-create_s3 <- function(user, project_shorthand,
-                      upload_api_key=Sys.getenv("STAGING_UPLOAD_API_KEY"),
-                      upload_env_name="staging",
-                      submission_n=0){
+create_s3 <- function(project_shorthand,
+                      hca_util_path = paste0(reticulate::virtualenv_root(),
+                                             "/ranglr_env/bin/hca-util"),
+                      permissions = NA, profile = NA){
 
-  full_dataset_name <- paste0(user, "-", project_shorthand, "-", upload_env_name,
-                              "-", submission_n)
-  print(full_dataset_name)
-  this_command <- paste0("python /data/tools/create_upload_area.py --api-key ",
-                          upload_api_key, " --dataset-name ", full_dataset_name,
-                          " --environment ", upload_env_name)
-  this_connection <- ssh_ec2(username = user)
-  out <- ssh::ssh_exec_internal(this_connection,
-                                command = this_command)
-  ssh::ssh_disconnect(this_connection)
-  s3_bucket_url <- stringr::str_split(rawToChar(out$stdout), "\n", simplify = T)[,5]
+  this_command <- paste0(hca_util_path, " create ", project_shorthand)
+  if(!is.na(permissions)){
+    this_command <- paste0(this_command, " -p ", permissions)
+  }
+  if(!is.na(profile)) {
+    this_command <- paste0(this_command, " --profile ", profile)
+  }
+  output = system(this_command, intern = TRUE)
+  print(output)
+  s3_bucket_url <- stringr::str_extract(output, pattern=".{8}-.{4}-.{4}-.{4}-.{12}")
   return(s3_bucket_url)
 }
 
-#' Copy s3 files between HCA s3 buckets
+#' Sync files between hca-util and ingest upload areas
 #'
-#' \code{copy_s3} copies files between two HCA s3 buckets. It will not work for
-#' buckets outside the HCA
+#' \code(sync_s3) copies files from an hca-util upload area into an ingest
+#' submission upload area
 #'
-#' @param{source_s3} the url of the s3 where the files to be copied are
-#' @param{dest_s3} the url of the s3 where you want the files to go
-#' @param{user} your username
+#' @param source_s3 the uuid of the hca-util upload area to copy from
+#' @param target_s3 the full address of the ingest submission upload area
+#' @param hca_util_path the path to installed hca-util
+#' @param profile specify name of admin profile, otherwise uses default
 #' @export
-copy_s3 <- function(source_s3, dest_s3, user){
-  this_connection <- ssh_ec2(username = user)
-  select_command <- paste0("hca upload select ", dest_s3)
-  upload_command <- paste0("hca upload files -s true ", source_s3)
-  out_select <-ssh::ssh_exec_internal(this_connection,
-                         command = select_command)
-  out_upload <- ssh::ssh_exec_internal(this_connection,
-                         command = upload_command)
-  ssh::ssh_disconnect(this_connection)
-  return(list(out_select, out_upload))
+sync_s3 <- function(source_s3, target_s3, profile = NA,
+                    hca_util_path = paste0(reticulate::virtualenv_root(),
+                                           "/ranglr_env/bin/hca-util")){
+  this_select_command <- paste0(hca_util_path, " select ", source_s3)
+  this_sync_command <- paste0(hca_util_path, " sync ", target_s3)
+  if(!is.na(profile)) {
+    this_sync_command <- paste0(this_sync_command, " --profile ", profile)
+  }
+  system(this_select_command)
+  system(this_sync_command)
 }
+
