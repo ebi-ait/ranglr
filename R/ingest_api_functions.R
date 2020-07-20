a2e6bf77fd4e96e4f2e02f89a3e8b08f7a19cfa9
mshadbolt <mshadbolt@ebi.ac.uk>
2020-07-20 12:40:46 +0100

    added audr functionality

--- R/ingest_api_functions.R
+++ R/ingest_api_functions.R
@@ -3,27 +3,26 @@
 #' \code{query_exising_submission} provides an easy to use wrapper to query the
 #' ingest api using a submission envelope uuid
 #'
-#' @param submission_id a string
-#' @param submission_environment either `staging` or `prod`
+#' @param submission_envelope the link to the submission envelope endpoint
+# @param submission_environment either `staging` or `prod`
 #' @param entity_type one of processes, projects, protocols, files, biomaterials
 #' @param size the number of results to return, 1000 by default
 #' @export
-query_existing_submission <- function(submission_id,
-                                      submission_environment = c("staging",
-                                                                 "prod"),
+query_existing_submission <- function(submission_envelope,
+                                      # submission_environment = c("staging",
+                                      #                            "prod"),
                                       entity_type = c("biomaterials", "files",
                                                       "processes", "projects",
                                                       "protocols"),
                                       size = 1000) {
-  if (submission_environment == "prod") {
-    submission_environment = ""
-  } else {
-    submission_environment <- paste0(".", "staging")
-  }
-  ingest_api_url <- paste0("https://api.ingest", submission_environment,
-                           ".data.humancellatlas.org/submissionEnvelopes/")
-  query_url <- paste0(ingest_api_url, submission_id, "/", entity_type,
-                      "?size=", size)
+  # if (submission_environment == "prod") {
+  #   submission_environment = ""
+  # } else {
+  #   submission_environment <- paste0(".", "staging")
+  # }
+  ingest_api_url <- paste0("https://api.ingest.archive.data.humancellatlas.org/submissionEnvelopes/")
+  query_url <- paste0(ingest_api_url, submission_envelope, "/",
+                      entity_type, "?size=", size)
   message(paste0("querying: ", query_url))
   request <- httr::GET(query_url, httr::timeout(60))
   if (request$status_code != 200) {
@@ -33,6 +32,32 @@ query_existing_submission <- function(submission_id,
   entity_uuid_df <- jsonlite::fromJSON(response,
                                        flatten = T)[["_embedded"]][[1]]
   return(entity_uuid_df)
+}
+
+#' Query ingest api to get submission envelope id from submission uuid
+#'
+#'@param submission_uuid the uuid for the submission
+#'@param submission_environment the environment the submission is in, one of "staging or "prod"
+#'@export
+get_submission_env_from_uuid <- function(sub_uuid,
+                                         sub_environment = c("staging",
+                                                                    "prod")){
+  if (sub_environment == "prod") {
+    sub_environment = ""
+  } else {
+    sub_environment <- paste0(".", "staging")
+  }
+  ingest_api_url <- paste0("https://api.ingest", sub_environment,
+                           ".data.humancellatlas.org/submissionEnvelopes/search/findByUuidUuid?uuid=")
+  query_url <- paste0(ingest_api_url, sub_uuid)
+  message(paste0("querying: ", query_url))
+  request <- httr::GET(query_url, httr::timeout(60))
+  if (request$status_code != 200) {
+    stop(paste0("API call failed with status ", request$status_code))
+  }
+  response <- httr::content(request, as = "text", encoding = "UTF-8")
+  submission_envelope <- jsonlite::fromJSON(response,flatten = T)[["_links"]]$submissionEnvelope$href
+  return(submission_envelope)
 }
 
 
